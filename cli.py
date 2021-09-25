90bbdecc41906eb8cf8ce90d73ae227848cb385b
IHimA2341 <ibrahimabdullah2341@gmail.com>
2021-09-25 13:53:34 +0100

    Updating 'cli.py'

--- cli.py
+++ cli.py
@@ -7,7 +7,7 @@ def connect():
         # reads the connection parameters.
         params = config()
 
-        # connects to the PostgreSQL server
+        # connects to the PostgreSQL server.
         print("Connecting to the PostgreSQL database...")
         conn = psycopg2.connect(**params)
 
@@ -15,7 +15,6 @@ def connect():
         cur = conn.cursor()
 
         return conn, cur
-
 
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
@@ -25,7 +24,6 @@ def connect():
 def close(conn):
     if conn is not None:
         conn.close()
-        print("Database connection closed.")
     
 
 def execute(statement=""):
@@ -41,20 +39,25 @@ def execute(statement=""):
             return
 
     
-        # Executes the statement
+        # Executes the statement.
         cursor.execute(statement)
         # Gets the response and prints the result.
         result = cursor.fetchall()
-        print(result)
+        return result
 
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
         return
 
     finally:
+        conn.commit()
         close(conn)
 
-def createdatabase():
+
+# Functions for the dictionary.
+
+# creates a new table called tasks.
+def createTable():
     execute(
         """
         CREATE TABLE IF NOT EXISTS tasks (
@@ -63,21 +66,80 @@ def createdatabase():
         status BOOLEAN NOT NULL
         );
         """)
-    print("Created a database for the task list.")
+    print("Created a table for the task list.")
 
-def showtasks():
-    return
+# formats the data from the SELECT statements.
+def formatData(data):
+    data_list = ""
+    for line in data:
+        data_list += line[0] + "\n"
+    
+    return data_list
 
+# shows the list of tasks that need doing.
+def showTodo():
+    task_list = execute("SELECT taskname, status FROM tasks WHERE status = FALSE;")
+    task_list = formatData(task_list)
+    final_str = "Things to do:\n" + task_list 
+    print(final_str)
 
+# shows the list of tasks that are completed.
+def showCompleted():
+    task_list = execute("SELECT taskname, status FROM tasks WHERE status = TRUE;")
+    task_list = formatData(task_list)
+    final_str = "Tasks that have been completed:\n" + task_list
+    print(final_str)
+
+# shows the full list of tasks.
+def showFullList():
+    task_list = execute("SELECT taskname, status FROM tasks;")
+    task_list = formatData(task_list)
+    final_str = "Here are the full list of tasks:\n" + task_list
+    print(final_str)
+
+# adds a task to the list.
+def addTask():
+    task_name = input("What is the task name? ")
+    execute("INSERT INTO tasks (taskname, status) VALUES ('{0}', FALSE);".format(task_name))
+
+# complete a task.
+def completeTask():
+    task_name = input("What is the task name? ")
+    execute("UPDATE tasks SET status = TRUE WHERE taskname = '{0}';".format(task_name))
+    
+# delete a task.
+def deleteTask():
+    task_name = input("What is the task name? ")
+    execute("DELETE FROM tasks WHERE taskname = '{0}'".format(task_name))
+
+# exits the program.
+def exitProgram():
+    print("Thank you for using this program.")
+    exit()
+
+# Action when operation is invalid.
+def invalidOp():
+    print("It seems your input wasn't recognised. Please try again.")
+
+def performoperation(chosen_ops):
+    
+    ops = {
+        "show todo": showTodo,
+        "show completed": showCompleted,
+        "create table": createTable,
+        "show list": showFullList,
+        "add task": addTask,
+        "complete task": completeTask,
+        "delete task": deleteTask,
+        "exit": exitProgram
+        }
+    
+    chosen_op_function = ops.get(chosen_ops, invalidOp)
+    return chosen_op_function()
+    
 if __name__ == "__main__":
     inputt = ""
-    while inputt != "exit":
+    
+    while True:
         inputt = input("What would you like to do? ")
-        if inputt.lower == "show todo":
-            print("okay")
-            execute("SELECT * FROM tasks WHERE status = FALSE")
-        elif inputt.lower == "create database":
-            createdatabase()
-        
-
-    print("End of program")
+        performoperation(inputt)
